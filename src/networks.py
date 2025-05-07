0d76176c711905645485125ebf08838443746c7d
modichirag <cmodi@flatironinstitute.org>
2025-05-07 12:01:57 -0400

    conditional Unet

--- src/networks.py
+++ src/networks.py
@@ -188,16 +188,19 @@ class UNetBlock(torch.nn.Module):
             self.qkv = Conv2d(in_channels=out_channels, out_channels=out_channels*3, kernel=1, gated=gated, **(init_attn if init_attn is not None else init))
             self.proj = Conv2d(in_channels=out_channels, out_channels=out_channels, kernel=1, gated=gated, **init_zero)
 
-    def forward(self, x, emb):
+    def forward(self, x, emb=None):
         orig = x
         x = self.conv0(silu(self.norm0(x)))
 
-        params = self.affine(emb).unsqueeze(2).unsqueeze(3).to(x.dtype)
-        if self.adaptive_scale:
-            scale, shift = params.chunk(chunks=2, dim=1)
-            x = silu(torch.addcmul(shift, self.norm1(x), scale + 1))
+        if emb is not None:
+            params = self.affine(emb).unsqueeze(2).unsqueeze(3).to(x.dtype)
+            if self.adaptive_scale:
+                scale, shift = params.chunk(chunks=2, dim=1)
+                x = silu(torch.addcmul(shift, self.norm1(x), scale + 1))
+            else:
+                x = silu(self.norm1(x.add_(params)))
         else:
-            x = silu(self.norm1(x.add_(params)))
+            x = self.norm1(x)
 
         x = self.conv1(torch.nn.functional.dropout(x, p=self.dropout, training=self.training))
         x = x.add_(self.skip(orig) if self.skip is not None else orig)
@@ -472,6 +475,125 @@ class DhariwalUNet(torch.nn.Module):
             emb = emb + self.map_label(tmp)
         emb = silu(emb)
 
+        # Encoder.
+        skips = []
+        for block in self.enc.values():
+            x = block(x, emb) if isinstance(block, UNetBlock) else block(x)
+            skips.append(x)
+
+        # Decoder.
+        for block in self.dec.values():
+            if x.shape[1] != block.in_channels:
+                x = torch.cat([x, skips.pop()], dim=1)
+            x = block(x, emb)
+        x = self.out_conv(silu(self.out_norm(x)))
+        return x
+
+#----------------------------------------------------------------------------
+# Reimplementation of the ADM architecture from the paper
+# "Diffusion Models Beat GANS on Image Synthesis". Equivalent to the
+# original implementation by Dhariwal and Nichol, available at
+# https://github.com/openai/guided-diffusion
+
+#@persistence.persistent_class
+class ConditionalDhariwalUNet(torch.nn.Module): #Difference in handling label_dim and class_labels, can be images
+    def __init__(self,
+        img_resolution,                     # Image resolution at input/output.
+        in_channels,                        # Number of color channels at input.
+        out_channels,                       # Number of color channels at output.
+        latent_dim          = None,          # Number of class labels, 0 = unconditional.
+        augment_dim         = 0,            # Augmentation label dimensionality, 0 = no augmentation.
+
+        model_channels      = 192,          # Base multiplier for the number of channels.
+        channel_mult        = [1,2,3,4],    # Per-resolution multipliers for the number of channels.
+        channel_mult_emb    = 4,            # Multiplier for the dimensionality of the embedding vector.
+        num_blocks          = 3,            # Number of residual blocks per resolution.
+        attn_resolutions    = [32,16,8],    # List of resolutions with self-attention.
+        dropout             = 0.10,         # List of resolutions with self-attention.
+        label_dropout       = 0,            # Dropout probability of class labels for classifier-free guidance.
+        gated               = False,         # Use gated convolutions? 
+        latent_channels     = 8 
+    ):
+        super().__init__()
+        self.label_dropout = label_dropout
+        emb_channels = model_channels * channel_mult_emb
+        init = dict(init_mode='kaiming_uniform', init_weight=np.sqrt(1/3), init_bias=np.sqrt(1/3))
+        init_zero = dict(init_mode='kaiming_uniform', init_weight=0, init_bias=0)
+        block_kwargs = dict(emb_channels=emb_channels, channels_per_head=64, dropout=dropout, init=init, init_zero=init_zero, gated=gated)
+
+        # Mapping.
+        self.map_noise = PositionalEmbedding(num_channels=model_channels)
+        self.map_augment = Linear(in_features=augment_dim, out_features=model_channels, bias=False, **init_zero) if augment_dim else None
+        self.map_layer0 = Linear(in_features=model_channels, out_features=emb_channels, **init)
+        self.map_layer1 = Linear(in_features=emb_channels, out_features=emb_channels, **init)
+        if latent_dim is None:
+            self.map_latents = None
+            latent_in_channels = 0
+        elif type(latent_dim) == int or type(latent_dim) == float:
+            self.map_latents = Linear(in_features=latent_dim, out_features=emb_channels, bias=False, init_mode='kaiming_normal', init_weight=np.sqrt(latent_dim))
+            latent_in_channels = 0
+        elif type(latent_dim) == list:
+            assert len(latent_dim) == 3
+            C, H, W = latent_dim
+            latent_in_channels = C
+            # self.map_label = UNetBlock(in_channels=C, out_channels=C, emb_channels=emb_channels, attention=False, dropout=0, init=init, init_zero=init_zero, gated=gated)
+            self.map_latents = torch.nn.Sequential(
+                Conv2d(in_channels=C, out_channels=latent_channels, kernel=3, gated=gated, **init),
+                UNetBlock(in_channels=latent_channels, out_channels=latent_channels, emb_channels=latent_channels, \
+                            attention=False, dropout=0, init=init, init_zero=init_zero, gated=gated),
+                Conv2d(in_channels=latent_channels, out_channels=C, kernel=3, gated=gated, **init)
+            )
+        else:
+            raise TypeError(f"label_dim must be None, int, float or list, but got {type(latent_dim)}")
+
+        # Encoder.
+        self.enc = torch.nn.ModuleDict()
+        cout = in_channels + latent_in_channels
+        for level, mult in enumerate(channel_mult):
+            res = img_resolution >> level
+            if level == 0:
+                cin = cout
+                cout = model_channels * mult
+                self.enc[f'{res}x{res}_conv'] = Conv2d(in_channels=cin, out_channels=cout, kernel=3, gated=gated, **init)
+            else:
+                self.enc[f'{res}x{res}_down'] = UNetBlock(in_channels=cout, out_channels=cout, down=True, **block_kwargs)
+            for idx in range(num_blocks):
+                cin = cout
+                cout = model_channels * mult
+                self.enc[f'{res}x{res}_block{idx}'] = UNetBlock(in_channels=cin, out_channels=cout, attention=(res in attn_resolutions), **block_kwargs)
+        skips = [block.out_channels for block in self.enc.values()]
+
+        # Decoder.
+        self.dec = torch.nn.ModuleDict()
+        for level, mult in reversed(list(enumerate(channel_mult))):
+            res = img_resolution >> level
+            if level == len(channel_mult) - 1:
+                self.dec[f'{res}x{res}_in0'] = UNetBlock(in_channels=cout, out_channels=cout, attention=True, **block_kwargs)
+                self.dec[f'{res}x{res}_in1'] = UNetBlock(in_channels=cout, out_channels=cout, **block_kwargs)
+            else:
+                self.dec[f'{res}x{res}_up'] = UNetBlock(in_channels=cout, out_channels=cout, up=True, **block_kwargs)
+            for idx in range(num_blocks + 1):
+                cin = cout + skips.pop()
+                cout = model_channels * mult
+                self.dec[f'{res}x{res}_block{idx}'] = UNetBlock(in_channels=cin, out_channels=cout, attention=(res in attn_resolutions), **block_kwargs)
+        self.out_norm = GroupNorm(num_channels=cout)
+        self.out_conv = Conv2d(in_channels=cout, out_channels=out_channels, kernel=3, gated=gated, **init_zero)
+
+
+    def forward(self, x, noise_labels, latents=None, augment_labels=None):
+        # Mapping.
+        emb = self.map_noise(noise_labels)
+        if self.map_augment is not None and augment_labels is not None:
+            emb = emb + self.map_augment(augment_labels)
+        emb = silu(self.map_layer0(emb))
+        emb = self.map_layer1(emb)
+        emb = silu(emb)
+
+        # if self.map_latents is not None:
+        if latents is not None:
+            latents = self.map_latents(latents)
+            x = torch.cat([x, latents], dim=1) 
+            
         # Encoder.
         skips = []
         for block in self.enc.values():
