a235111bcc025ec9f7a074834576bb4e95ae4ed4
modichirag <cmodi@flatironinstitute.org>
2025-05-24 14:49:24 -0400

    updates

--- job-cifar10-dist.sh
+++ job-cifar10-dist.sh
@@ -6,7 +6,7 @@
 #SBATCH --constraint=h100
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 #SBATCH -o logs/r%x.o%j
 
 export OMP_NUM_THREADS=1
@@ -17,10 +17,11 @@ source /mnt/home/cmodi/envs/torchlatest/bin/activate
 clevel=$1
 nlevel=$2
 corruption="random_mask"
+# corruption="block_mask"
 # corruption="gaussian_blur"
 
 torchrun --standalone --nproc_per_node=2  deconvolving_distributed.py  \
-                    --dataset cifar10 --corruption $corruption --corruption_level $clevel $nlevel \
-                    --train_steps 2_000 --channels 64 --batch_size 128  --learning_rate 0.001  \
-                    --suffix condtest
+        --dataset cifar10 --corruption $corruption --corruption_level $clevel $nlevel \
+        --train_steps 5_000 --channels 64 --batch_size 256 --learning_rate 0.001  \
+        --suffix condtest --lr_scheduler 
 
