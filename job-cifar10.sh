f15675e7c6edc787e1b214d7e7eca58270af454f
modichirag <cmodi@flatironinstitute.org>
2025-05-24 14:48:07 -0400

    update job scripts;

--- job-cifar10.sh
+++ job-cifar10.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH --ntasks=1
-#SBATCH --mem=32G
 #SBATCH --nodes=1
-#SBATCH --constraint=h100
+#SBATCH --constraint=a100
 #SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=0:20:00
+#SBATCH --cpus-per-task=2
+#SBATCH --time=72:00:00
 #SBATCH -o logs/r%x.o%j
 
 export OMP_NUM_THREADS=1
@@ -14,5 +13,21 @@ export OMP_NUM_THREADS=1
 module load modules/2.3-20240529
 source /mnt/home/cmodi/envs/torchlatest/bin/activate
 
-#time torchrun run.py --config configs/finewebmini.yaml
-time python -u train_cifar10.py
+corruption=$1
+clevel=$2
+nlevel=$3
+mlevel=$4
+echo $corruption
+echo $clevel
+echo $nlevel
+
+dataset='cifar10'
+# dataset='celebA'
+channels=64
+trainsteps=50_000
+time python -u deconvolving_interpolants.py \
+                 --dataset $dataset --corruption $corruption \
+                --corruption_level $clevel $nlevel $mlevel --train_steps $trainsteps \
+                --channels $channels  --ode_steps 64 --alpha 0.9 --resamples 2  \
+                --lr_scheduler --suffix "v3" --learning_rate 0.0005
+
--- job-sample.sh
+++ job-sample.sh
@@ -6,7 +6,7 @@
 #SBATCH --constraint=a100
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 #SBATCH -o logs/r%x.o%j
 
 export OMP_NUM_THREADS=1
@@ -14,4 +14,7 @@ export OMP_NUM_THREADS=1
 module load modules/2.3-20240529
 source /mnt/home/cmodi/envs/torchlatest/bin/activate
 
-time python -u sample.py --folder mnist-smin0.10 --sigma_min 0.05 --num_samples 5120 --dataset mnist --sde_sampling
+smin=$1
+echo $smin
+# time python -u sample.py --folder mnist-smin0.10 --sigma_min 0.05 --num_samples 5120 --dataset mnist --sde_sampling
+time python -u sample.py --folder cifar10-smin0.50 --sigma_min $smin --num_samples 10240 --dataset cifar10 --channels 96
